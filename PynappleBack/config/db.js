cd34cbac41ae3a9eea48ce719638c680337b812f
SUDHANVA-16 <sudhanva1744@gmail.com>
2025-09-07 16:56:43 +0530

    Fix Backend

--- PynappleBack/config/db.js
+++ PynappleBack/config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const url = process.env.MONGO_URL;
-// mongoose.connect('mongodb+srv://Sudhanva_77:5OejLfUuZLz1GbBB@cluster0.r0ik23t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 mongoose.connect(url, {
     useNewUrlParser: true,
